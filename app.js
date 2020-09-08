b0a1b7e139bdba336df1636bda40ddd035d0fbaf
msudol <matt.sudol@gmail.com>
2020-09-08 00:34:33 -0400

    stubs

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * node-webcrawler - As a school assignment, develop a utility to crawl a website that will fetch the index page and perform a series of defined actions with the data.
+ * @license MIT
+ *
+ *  https://github.com/msudol/node-webcrawler
+ */
+ 
+'use strict';
+
+// get config values
+const config = require('./config/config.js');
---
+++ config/config.js
@@ -0,0 +1,11 @@
+// config.js
+
+'use strict';
+
+// Static config, do not edit
+let config = {};
+
+// CLI prefix
+config.prefix = 'CMD> ';
+
+module.exports = config;
